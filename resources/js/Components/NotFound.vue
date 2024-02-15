44415046abdf55db349b7d3aaccb2163e1417a29
Jhon Alonso <jhon2alonso@gmail.com>
2024-02-15 17:13:58 -0500

    cambio en componente

--- resources/js/Components/NotFound.vue
+++ resources/js/Components/NotFound.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <h1>not found component</h1>
     <br />
-    <router-link to="/">Back to view</router-link>
+    <router-link to="/">Back to view home</router-link>
 </template>
 <script setup></script>
 <style lang=""></style>
